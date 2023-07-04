34d48313c9e0ed52f2fb94ce1f784f53ef029752
ilyasosman <earlyosman@gmail.com>
2023-07-04 10:26:17 +0300

    struct fraction

---
+++ Chapter-10-compound-types-enums-and-structs/quiz2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+struct Fraction 
+{
+    int numerator {};
+    int denominator {};
+};
+
+Fraction getFraction() 
+{
+    Fraction temp;
+
+    std::cout << "Enter a value for the numerator: " << '\n';
+    std::cin >> temp.numerator;
+
+    std::cout << "Enter a value for the denominator: " << '\n';
+    std::cin >> temp.denominator;
+
+    return temp;
+}
+
+Fraction multiplyFraction (const Fraction& fraction1, const Fraction& fraction2)
+{
+    return { fraction1.numerator * fraction2.numerator, fraction1.denominator * fraction2.denominator };
+}
+
+void print(const Fraction& f) {
+    std::cout << f.numerator << '/' << f.denominator;
+}
+
+
+int main()
+{
+    Fraction fractionOne { getFraction() };
+    Fraction fractionTwo { getFraction() };
+
+    print(multiplyFraction(fractionOne, fractionTwo));
+
+    return 0;
+}
