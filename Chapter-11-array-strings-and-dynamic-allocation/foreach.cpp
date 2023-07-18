db1c42dafebc23fc447929335028ed6471bcfec3
ilyasosman <earlyosman@gmail.com>
2023-07-18 21:22:07 +0300

    foreach "

---
+++ Chapter-11-array-strings-and-dynamic-allocation/foreach.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+int main()
+{
+    constexpr std::string_view names[]{ "Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly" };
+
+    std::cout << "Enter a name: ";
+    std::string username{};
+    std::cin >> username;
+
+    bool found{ false };
+
+    // We will be explicit about expecting `name` to be a std::string_view here
+    // That way if `names` is ever changed to an expensive to copy type (like std::string)
+    // We won't end up making expensive copies.
+    for (std::string_view name : names)
+    {
+        if (name == username)
+        {
+            found = true;
+            break;
+        }
+    }
+
+    if (found)
+        std::cout << username << " was found.\n";
+    else
+        std::cout << username << " was not found.\n";
+
+    return 0;
+}
