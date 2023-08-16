e5656995cf9ae1a7dc633f88670ceee300613288
ilyasosman <earlyosman@gmail.com>
2023-08-16 07:24:58 +0300

    fraction / overload

--- chapter-14-operator-overload/Fraction.cpp
+++ chapter-14-operator-overload/Fraction.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+#include <numeric> // for std::gcd
 
+// This version of the Fraction class auto-reduces fractions
 class Fraction
 {
 private:
-	int m_numerator { 0 };
-	int m_denominator { 1 };
+	int m_numerator{ 0 };
+	int m_denominator{ 1 };
 
 public:
-	Fraction(int numerator, int denominator=1)
-		: m_numerator{numerator}, m_denominator{denominator}
+	Fraction(int numerator, int denominator = 1)
+		: m_numerator{ numerator }, m_denominator{ denominator }
 	{
+		// We put reduce() in the constructor to ensure any fractions we make get reduced!
+		// Since all of the overloaded operators create new Fractions, we can guarantee this will get called here
+		reduce();
 	}
 
-	// We don't want to pass by value, because copying is slow.
-	// We can't and shouldn't pass by non-const reference, because then
-	// our functions wouldn't work with r-values.
+	void reduce()
+	{
+		int gcd{ std::gcd(m_numerator, m_denominator) };
+		if (gcd) // Make sure we don't try to divide by 0
+		{
+			m_numerator /= gcd;
+			m_denominator /= gcd;
+		}
+	}
+
 	friend Fraction operator*(const Fraction& f1, const Fraction& f2);
 	friend Fraction operator*(const Fraction& f1, int value);
 	friend Fraction operator*(int value, const Fraction& f1);
@@ -42,10 +54,10 @@ Fraction operator*(int value, const Fraction& f1)
 
 int main()
 {
-	Fraction f1{2, 5};
+	Fraction f1{ 2, 5 };
 	f1.print();
 
-	Fraction f2{3, 8};
+	Fraction f2{ 3, 8 };
 	f2.print();
 
 	Fraction f3{ f1 * f2 };
@@ -59,6 +71,9 @@ int main()
 
 	Fraction f6{ Fraction{1, 2} * Fraction{2, 3} * Fraction{3, 4} };
 	f6.print();
+
+	Fraction f7{ 0, 6 };
+	f7.print();
 
 	return 0;
 }
