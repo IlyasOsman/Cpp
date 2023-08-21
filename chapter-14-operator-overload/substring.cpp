6d6eb04afddd42123335b88d223df0b0ce45d87f
ilyasosman <earlyosman@gmail.com>
2023-08-21 21:35:08 +0300

    substring

---
+++ chapter-14-operator-overload/substring.cpp
@@ -0,0 +1,40 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+class MyString
+{
+private:
+    std::string m_string{};
+
+public:
+    MyString(const std::string &string = {})
+        : m_string{string}
+    {
+    }
+
+    MyString operator()(int start, int length)
+    {
+        assert(start >= 0);
+        assert(start + length <= static_cast<int>(m_string.length()) && "MyString::operator(int, int): Substring is out of range");
+
+        return m_string.substr(
+            static_cast<std::string::size_type>(start),
+            static_cast<std::string::size_type>(length));
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const MyString &s)
+    {
+        out << s.m_string;
+
+        return out;
+    }
+};
+
+int main()
+{
+    MyString s{"Hello, world!"};
+    std::cout << s(7, 5) << '\n'; // start at index 7 and return 5 characters
+
+    return 0;
+}
